413ddf4f8ff58a44d73a42cd0a9a747b46792668
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-05-08 22:31:09 -0700

    Prueba
    Prueba

---
+++ CUNutricional_DTO/src/main/java/enriquemadridalvarez/cunutricional_dto/CUNutricional_DTO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package enriquemadridalvarez.cunutricional_dto;
+
+/**
+ *
+ * @author BALAMRUSH
+ */
+public class CUNutricional_DTO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
