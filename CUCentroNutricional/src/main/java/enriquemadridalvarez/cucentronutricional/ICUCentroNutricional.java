f722cb6592a1fa10b695f90f7e8cb30315e49363
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-05-11 20:30:16 -0700

    ICUCentroNutricional Subistema Avance

--- CUCentroNutricional/src/main/java/enriquemadridalvarez/cucentronutricional/ICUCentroNutricional.java
+++ CUCentroNutricional/src/main/java/enriquemadridalvarez/cucentronutricional/ICUCentroNutricional.java
@@ -10,6 +10,7 @@ import com.mycompany.fitlifegym_dtos.DatosBiologicosDTO;
 import com.mycompany.fitlifegym_dtos.EstiloVidaDTO;
 import com.mycompany.fitlifegym_dtos.PacienteDTO;
 import com.mycompany.fitlifegym_dtos.PadecimientoDTO;
+import com.mycompany.fitlifegym_dtos.RegistroComidaDTO;
 import com.mycompany.fitlifegym_dtos.UnidadMedidaDTO;
 import com.mycompany.fitlifegym_negocio.NegocioException;
 import java.util.List;
@@ -19,21 +20,38 @@ import java.util.List;
  * @author BALAMRUSH
  */
 public interface ICUCentroNutricional {
-    public abstract boolean validarPacienteExistente() throws NegocioException;
-    public abstract boolean validarEstiloVida() throws NegocioException;
-    public abstract boolean validarDatosBiologicos() throws NegocioException;
-    public abstract boolean validarAlergia() throws NegocioException;
-    public abstract boolean validarAlimento() throws NegocioException;
+    
+    public abstract boolean validarPacienteExistente(PacienteDTO paciente) throws NegocioException;
+    
+    public abstract boolean validarEstiloVida(EstiloVidaDTO estiloVida) throws NegocioException;
+    
+    public abstract boolean validarDatosBiologicos(DatosBiologicosDTO datosBiologicos) throws NegocioException;
+    
+    public abstract boolean validarAlergia(AlergiaDTO alergia) throws NegocioException;
+    
+    public abstract boolean validarAlimento(AlimentoDTO alimento) throws NegocioException;
+    
     public abstract boolean validarCantidad() throws NegocioException;
-    public abstract boolean validarUnidadMedida() throws NegocioException;
-    public abstract boolean validarPadecimiento() throws NegocioException;
+    
+    public abstract boolean validarUnidadMedida(UnidadMedidaDTO unidadMedida) throws NegocioException;
+    
+    public abstract boolean validarPadecimiento(PadecimientoDTO padecimiento) throws NegocioException;
+    
     public abstract boolean validarDiaSemana() throws NegocioException;
-    public abstract boolean validarRegistroDieta() throws NegocioException;
+    
+    public abstract boolean validarRegistroDieta(RegistroComidaDTO registroComida) throws NegocioException;
+    
     public abstract List<PacienteDTO> consultarPaciente() throws NegocioException;
+    
     public abstract List<EstiloVidaDTO> consultarEstiloVida() throws NegocioException;
+    
     public abstract List<DatosBiologicosDTO> consultarDatosBiologicos() throws NegocioException;
+    
     public abstract List<AlergiaDTO> consultarAlergia() throws NegocioException;
+    
     public abstract List<AlimentoDTO> consultarAlimento() throws NegocioException;
+    
     public abstract List<UnidadMedidaDTO> consultarUnidadMedida() throws NegocioException;
+    
     public abstract List<PadecimientoDTO> consultarPadecimiento() throws NegocioException;
 }
