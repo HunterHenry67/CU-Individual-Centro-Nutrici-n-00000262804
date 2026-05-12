1367ee37c8ce9b80ff6783b5afad2af76d113100
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-05-11 17:53:56 -0700

    Subsistema CUCentroNutricional

---
+++ CUCentroNutricional/src/main/java/enriquemadridalvarez/cucentronutricional/ICUCentroNutricional.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package enriquemadridalvarez.cucentronutricional;
+
+import enriquemadridalvarez.cunutricional_negocio.NegocioException;
+import java.util.List;
+
+/**
+ *
+ * @author BALAMRUSH
+ */
+public interface ICUCentroNutricional {
+    public abstract boolean validarPacienteExistente() throws NegocioException;
+    public abstract boolean validarEstiloVida() throws NegocioException;
+    public abstract boolean validarDatosBiologicos() throws NegocioException;
+    public abstract boolean validarAlergia() throws NegocioException;
+    public abstract boolean validarAlimento() throws NegocioException;
+    public abstract boolean validarCantidad() throws NegocioException;
+    public abstract boolean validarUnidadMedida() throws NegocioException;
+    public abstract boolean validarPadecimiento() throws NegocioException;
+    public abstract boolean validarDiaSemana() throws NegocioException;
+    public abstract boolean validarRegistroDieta() throws NegocioException;
+    public abstract List<PacienteDTO> consultarPaciente() throws NegocioException;
+}
