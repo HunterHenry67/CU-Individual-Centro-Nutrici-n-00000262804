f7991d19df85091c54b194f8f76e08f10e7929da
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-05-12 23:06:36 -0700

    Actualización IPersistenciaDAO y PersistenciaDAO

--- FitLifeGYM_Proyecto-main/FitLifeGYM_Persistencia/src/main/java/com/mycompany/fitlifegym_persistencia/IPersistenciaDAO.java
+++ FitLifeGYM_Proyecto-main/FitLifeGYM_Persistencia/src/main/java/com/mycompany/fitlifegym_persistencia/IPersistenciaDAO.java
@@ -4,18 +4,60 @@
  */
 package com.mycompany.fitlifegym_persistencia;
 
+import com.mycompany.fitlifegym_persistencia.entidades.Alergia;
+import com.mycompany.fitlifegym_persistencia.entidades.Alimento;
+import com.mycompany.fitlifegym_persistencia.entidades.DatosBiologicos;
+import com.mycompany.fitlifegym_persistencia.entidades.Dieta;
+import com.mycompany.fitlifegym_persistencia.entidades.EstiloVida;
+import com.mycompany.fitlifegym_persistencia.entidades.Paciente;
+import com.mycompany.fitlifegym_persistencia.entidades.Padecimiento;
+import com.mycompany.fitlifegym_persistencia.entidades.UnidadMedida;
+import java.util.List;
+
 /**
  *
  * @author BALAMRUSH
  */
 public interface IPersistenciaDAO {
-    public abstract IPacienteDAO getPacienteDAO();
-    public abstract IAlergiaDAO getAlergiaDAO();
-    public abstract IAlimentoDAO getAlimentosDAO();
-    public abstract IClientesDAO getClientesDAO();
-    public abstract IDatosBiologicosDAO getDatosBiologicosDAO();
-    public abstract IDietaDAO getDietasDAO();
-    public abstract IEstiloVidaDAO getEstiloVidaDAO();
-    public abstract IPadecimientoDAO getPadecimientoDAO();
-    public abstract IRegistroDietaDAO getRegistroDietaDAO();
+
+    public abstract Paciente consultarPaciente(Long idPaciente) throws PersistenciaException;
+
+    public abstract List<DatosBiologicos> consultarDatosBiologicos(Long idPaciente) throws PersistenciaException;
+
+    public abstract boolean eliminarPaciente(Long idPaciente) throws PersistenciaException;
+
+    public abstract DatosBiologicos registrarDatosBiologicos(DatosBiologicos datosBiologicos) throws PersistenciaException;
+
+    public abstract Alergia agregarAlergia(Alergia alergia) throws PersistenciaException;
+
+    public abstract List<Alergia> consultarAlergia() throws PersistenciaException;
+
+    public abstract boolean eliminarAlergia(Long idAlergia) throws PersistenciaException;
+
+    public abstract Padecimiento agregarPadecimiento(Padecimiento padecimiento) throws PersistenciaException;
+
+    public abstract List<Padecimiento> consultarPadecimiento(Long idPadecimiento) throws PersistenciaException;
+
+    public abstract boolean eliminarPadecimiento(Long idPadecimiento) throws PersistenciaException;
+
+    public abstract EstiloVida seleccionarEstiloVida(EstiloVida estiloVida) throws PersistenciaException;
+
+    public abstract List<EstiloVida> consultarEstiloVida(Long idEstiloVida) throws PersistenciaException;
+
+    public abstract Alimento agregarAlimento(Alimento alimento) throws PersistenciaException;
+
+    public abstract List<Alimento> consultarAlimento(Long idAlimento) throws PersistenciaException;
+
+    public abstract UnidadMedida agregarUnidadMedida(UnidadMedida unidadMedida) throws PersistenciaException;
+
+    public abstract List<UnidadMedida> consultarUnidadMedida(Long idUnidadMedida) throws PersistenciaException;
+
+    public abstract Dieta agregarRegistroDieta(Dieta dieta) throws PersistenciaException;
+
+    public abstract Dieta registrarDieta(Dieta dieta) throws PersistenciaException;
+
+    public abstract List<Dieta> busquedaDietaPacienteFiltro(Long idDieta) throws PersistenciaException;
+
+    public abstract List<Dieta> buscarDietaFiltro(Long idDieta) throws PersistenciaException;
+
 }
